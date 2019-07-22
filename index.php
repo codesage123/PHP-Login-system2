1250ab936ab698447948e9b39dda9e237f0de940
codesage123 <sagestephens9717@gmail.com>
2019-07-22 23:42:03 +0200

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-<?php echo "hello world";?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/css/uikit.min.css" />
+</head>
+<body>
+<div class="uk-section uk-container ">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+    <form class="uk-form-stacked js-login">
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Email</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+    </div>
+</div>
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Password</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+    </div>
+</div>
+
+
+<div class="uk-margin">
+    <button class="uk-button uk-button-defualt" type="submit">Login</button>
+</div>
+
+</form>
+    </div>      
+</div>
+
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.6/js/uikit-icons.min.js"></script>
+
+ 
+</body>
+</html>
